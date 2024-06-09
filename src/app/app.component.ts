c82fee82cb0b2a3efc9654f8c05ce6cf73b07510
Tamta <tamtakopadz16@gmail.com>
2024-06-09 14:33:11 +0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'task-manager-front';
+  title: string = 'task-manager-front';
 }
