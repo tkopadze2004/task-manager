d99c585e3746e00fbe92e57f2f993812febf2c43
Tamta <tamtakopadz16@gmail.com>
2024-06-16 20:21:41 +0400

    remove type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title: string = 'task-manager-front';
+  title = 'task-manager-front';
 }
