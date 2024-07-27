f2c74bfe921fe6eaa35c4f3af6c53fb43e02ff69
Tamta <tamtakopadz16@gmail.com>
2024-07-27 18:42:11 +0400

    added roles route and component outline

---
+++ src/app/pages/roles/roles.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RolesComponent } from './roles.component';
+
+export const rolesRoutes: Routes = [
+  {
+    path:'',
+    component:RolesComponent
+  }
+];
