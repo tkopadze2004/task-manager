081e973d8de02907de8807db45f68317aebbd336
Tamta <tamtakopadz16@gmail.com>
2024-08-06 13:11:01 +0400

    update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api/',
+  production: true,
+  apiUrl: 'https://task-manager-backend-production-ae80.up.railway.app/api/',
+  // apiUrl: 'http://localhost:3000/api/',
 };
