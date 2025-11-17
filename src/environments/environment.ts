90ac9a188d84fe1d9365aad7a78f4ac41e5ea2ac
Tamta <tamtakopadz16@gmail.com>
2025-11-18 00:50:04 +0400

    refactor: update apiUrl for production and development environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://task-manager-backend-production-6211.up.railway.app/api/',
+  apiUrl: 'https://task-manager-backend-production-8ee6.up.railway.app/api/',
   // apiUrl: 'http://localhost:3000/api/',
 };
